7c733ec68965aade877b757bda048ebe2bf22da2
zhounianlai <zhounianlai@teacher.com.cn>
2019-12-12 21:44:32 +0800

    解决移动端点击延迟200ms的问题 适配

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import router from "./router/index";
 import store from './store/index';
 import config from "./config";
 import useVant from "./uiComponents/vant/index";
-import { httpJson, httpForm, httpMultipart } from "./libs/http";
+import FastClick from 'fastclick'
+import api from './api/index';
+// 全局引入rem
+import './libs/rem';
+// import { httpJson, httpForm, httpMultipart } from "./libs/http";
 import $cookie from "./storage/cookie";
 import $localStorage from "./storage/localStorage";
 import $sessionStorage from "./storage/sessionStorage";
@@ -14,6 +18,15 @@ import "vant/lib/icon/local.css";
 // TODO: 真机调试时使用插件，切记打包上线时注释掉！！！！！！！！！
 // import VConsole from "vconsole";
 // var vConsole = new VConsole();
+
+// 解决移动端点击延迟200ms的问题
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function () {
+        FastClick.attach(document.body);
+    }, false);
+}
+
+Vue.use(api)
 
 Vue.config.productionTip = false
 
@@ -27,13 +40,13 @@ globalRegister(Vue);
 Vue.prototype.$config = config;
 
 // Global request method of params format is JSON.
-Vue.prototype.$httpJson = httpJson;
+// Vue.prototype.$httpJson = httpJson;
 
-// Global request method of params format is FormData.
-Vue.prototype.$httpForm = httpForm;
+// // Global request method of params format is FormData.
+// Vue.prototype.$httpForm = httpForm;
 
-// Global request method of params format is multipart.
-Vue.prototype.$httpMultipart = httpMultipart;
+// // Global request method of params format is multipart.
+// Vue.prototype.$httpMultipart = httpMultipart;
 
 // Global cookie method $cookie.
 Vue.prototype.$cookie = $cookie;
